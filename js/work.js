d60bbbba56bcd16eeca51a534a2c471591d6b9c3
P4RT33K <parteekpar00@gmail.com>
2021-07-16 13:47:51 -0400

    Changes in .html, .css and .js files!

--- js/work.js
+++ js/work.js
@@ -1,1 +1,11 @@
 // for Javascript files, if needed
+const name = "Parteek";
+
+function logger(){
+    console.log("Party time!");
+    console.log("Party time!");
+    console.log("Party time!");
+    console.log("Party time!");
+}
+
+logger();
