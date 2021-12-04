88216dde48390ae7cc904b80853a766fdce09f5a
P4RT33K <parteekpar00@gmail.com>
2021-12-04 01:46:39 -0500

    Navigation menu working!

--- js/script.js
+++ js/script.js
@@ -10,10 +10,12 @@
 
     function showNavMenu(){
         navMenu.classList.add("open");
+        bodyScrollingToggle();
     }
     function hideNavMenu(){
         navMenu.classList.remove("open");
         fadeOutEffect();
+        bodyScrollingToggle();
     }
     function fadeOutEffect(){
         document.querySelector(".fade-out-effect").classList.add("active");
@@ -21,6 +23,48 @@
             document.querySelector(".fade-out-effect").classList.remove("active");
         }, 300)
     }
+
+    // attach an event handler to document
+    document.addEventListener("click", () =>{
+        if(event.target.classList.contains('link-item')){
+            //make sure event.target.hash has a value overriding default behaviour
+            if(event.target.hash !==""){
+                //prevent default anchor click behaviour
+                event.preventDefault();
+                const hash = event.target.hash;
+                // deactivates existing active section
+                document.querySelector(".section.active").classList.add("hide");
+                document.querySelector(".section.active").classList.remove("active");
+                // activates new section
+                document.querySelector(hash).classList.add("active");
+                document.querySelector(hash).classList.remove("hide");
+                // deactivating existing active navigation menu 'link-item'
+                navMenu.querySelector(".active").classList.add("outer-shadow", "hover-in-shadow");
+                navMenu.querySelector(".active").classList.remove("active", "inner-shadow");
+                // if clicked link-item is contained within navigation menu
+                if(navMenu.classList.contains("open")){
+                    // activating new navigation menu 'link-item'
+                    event.target.classList.add("active", "inner-shadow");
+                    event.target.classList.remove("outer-shadow", "hover-in-shadow");
+                    // hide navigation MENU
+                    hideNavMenu();
+                }
+                else{
+                    let navItems = navMenu.querySelectorAll(".link-item");
+                    navItems.forEach((item) => {
+                        if(hash === item.hash){
+                            // activate new navigation menu link-item
+                            item.classList.add("active", "inner-shadow");
+                            item.target.classList.remove("outer-shadow", "hover-in-shadow");
+                        }
+                    })
+                    fadeOutEffect();
+                }
+                // add hash(#) to url
+                window.location.hash = hash;
+            }
+        }
+    })
 })();
 
 /* ==  ABOUT SECTION TABS  == */
