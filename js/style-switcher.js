ba5193e1a9eda5cf5aef2b817947cb4392a6ad7a
P4RT33K <parteekpar00@gmail.com>
2021-12-06 02:38:15 -0500

    Style switcher in progress!

---
+++ js/style-switcher.js
@@ -0,0 +1,12 @@
+/*  == TOGGLE STYLE SWITCHER ==  */
+const styleSwitcherToggler = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggler.addEventListener("click", () =>{
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
+
+/*  == HIDE STYLE SWITCHER ON SCROLL ==  */
+window.addEventListener("scroll", () =>{
+    if(document.querySelector(".style-switcher").classList.contains("open")){
+        document.querySelector(".style-switcher").classList.remove("open")
+    }
+})
